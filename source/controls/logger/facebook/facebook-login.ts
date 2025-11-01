5fa95691d1211fcd20b709fb5d6e143cd5ba60b4
Francis Loyd Raval 2 <officialfrancisloydraval@gmail.com>
2025-11-01 18:04:10 +0000

    In-dev(fca): Implementation of biar-fca.

--- source/controls/logger/facebook/facebook-login.ts
+++ source/controls/logger/facebook/facebook-login.ts
@@ -1,39 +1,33 @@
-import login from "@dongdev/fca-unofficial";
-import { readFileSync } from "fs-extra";
-import { log } from "@sy-log";
+import { login } from "biar-fca";
+import * as fs from "fs";
 import listener from "@sy-listener";
+import { log } from "@sy-log";
 
-let credentials;
-
-try {
-  credentials = { appState: JSON.parse(readFileSync("./appstate.json", "utf8")) };
-} catch (error) {
-  log("ERROR", "appstate.json is missing or malformed.");
-  process.exit(1);
-}
+const credentials = {
+  appState: JSON.parse(fs.readFileSync("appstate.json", "utf8"))
+};
 
 export async function facebookLogin() {
-  const { config } = globalThis.Sypher;
-  login(credentials, (err, api) => {
-    api.setOptions({
-      listenEvents: config.fcaOptions.listenEvents,
-      selfListen: config.fcaOptions.selfListen,
-      autoMarkDelivery: config.fcaOptions.autoMarkDelivery,
-      autoMarkRead: config.fcaOptions.autoMarkRead,
-      userAgent: config.fcaOptions.userAgent,
-    });
-    try {
-      api.listenMqtt((error, event) => {
-        if (error) {
-          if (error === "Connection closed.") {
-            console.error(`Error during API listen: ${error}`);
+  login(credentials, {
+    updatePresence: true,
+    autoMarkRead: true
+  }, (err: any, api: any) => {
+    if (err) return console.error(err);
+
+    log("FACEBOOK", "Bot online with protection!");
+
+    api.listenMqtt((err: any, event: any) => {
+      try {
+        if (err) {
+          if (err === "Connection closed.") {
+            console.error(`Error during API listen: ${err}`);
           }
-          console.log(error);
+          console.log(err);
         }
         listener({ api, event });
-      });
-    } catch (error) {
-      log("ERROR", "Error whilst listening: " + error);
-    }
+      } catch (error: any) {
+        console.log("ERROR", "Error whilst listening: " + error);
+      }
+    });
   });
 }
