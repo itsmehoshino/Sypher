93066b371e67af107e9584872a43e10068ded98d
Francis Loyd Raval 2 <officialfrancisloydraval@gmail.com>
2025-11-02 22:16:34 +0800

    Update starter.ts

--- source/controls/plugins/starter.ts
+++ source/controls/plugins/starter.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "temp")));
 
 app.get("/health", (req, res) => {
   res.json({
